3fc0207baff42d6aa916c5a62e4a860377cfdeb2
Boris Skurikhin <boriskurikhin@gmail.com>
2020-12-03 00:21:13 -0500

    add day 3 code

---
+++ day3.py
@@ -0,0 +1,19 @@
+count = 0
+with open('input.txt') as inp:
+    m = inp.readlines()
+    i, j, t = 0, 0, 1
+    for k in [[1, 1], [3, 1], [5,1], [7,1], [1, 2]]:
+        tree = 0
+        i = 0
+        j = 0
+        while i < len(m):
+            print(i, j)
+            if m[i][j] == '#':
+                tree += 1
+            i += k[1]
+            j = (j + k[0]) % (len(m[0]) - 1)
+        print('tree is ', tree)
+        t *= tree
+    print(t)
+    
+print(count)

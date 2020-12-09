17c047ff4d9156b89af489c79dd64fa50e53094e
Boris Skurikhin <boriskurikhin@gmail.com>
2020-12-09 00:49:24 -0500

    fix stuff

--- day9.py
+++ day9.py
@@ -2,11 +2,10 @@
 with open('input.txt') as ___:
     lines = list(map(lambda x: int(x.strip()), ___.readlines()))
     from collections import deque
-    pre, prefix, cache = [], [lines[0]], deque()
+    prefix, cache = [lines[0]], deque()
     N, L = 25, 0
     
     for i in range(N):
-        pre.append(lines[i])
         cache.append(lines[i])
         prefix.append(lines[i] + prefix[-1])
         L += 1
@@ -19,23 +18,19 @@ with open('input.txt') as ___:
                 good = True
                 break
         
-        L += 1
-        cache.popleft()
-        cache.append(lines[i])
-
         if not good:
             print('1: ', lines[i])
-            for left in range(0, L):
+            for left in range(1, i):
                 mini, maxi = lines[left], lines[left]
-                for right in range(left + 1, L):
+                for right in range(left + 1, i):
                     mini = min(mini, lines[right])
                     maxi = max(maxi, lines[right])
-
-                    if prefix[right] - prefix[left] + lines[right] == lines[i]:
+                    if prefix[right] - prefix[left] == lines[i]:
                         print('2: ', mini + maxi)
                         exit(0)
-                        
-        pre.append(lines[i])
+        
+        cache.popleft()
+        cache.append(lines[i])
         prefix.append(lines[i] + prefix[-1])
             
                 

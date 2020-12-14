35c4097d06c5ffc21728fd7d0af70adf537ee5dc
Boris Skurikhin <boriskurikhin@gmail.com>
2020-12-14 01:56:53 -0500

    day 14 sol

---
+++ day14.py
@@ -0,0 +1,33 @@
+# bitmask math
+# had some difficulty understanding the question today
+# but we solved it at last
+
+with open('input.txt') as ___:
+    lines = list(map(lambda x: x.strip(), ___.readlines()))
+    bitMask = 0
+    memory = {}
+    for i in range(len(lines)):
+        L = lines[i]
+        if L.startswith('mask'):
+            m = L[7:][::-1]
+            bitMask = m
+        else:
+            address = int(L[L.index('[') + 1:L.index(']')])
+            va = L.split(' = ')
+            value = int(va[1])
+            quantum = []
+
+            # apply mask            
+            for i in range(len(bitMask)):
+                if bitMask[i] == 'X': quantum.append(i) 
+                else: address |= (int(bitMask[i]) << i)
+
+            # powerset
+            N = 2 ** len(quantum)
+            for count in range(N):
+                for k in range(len(quantum)):
+                    if count & (1 << k): address |= (1 << quantum[k])
+                    else: address &= ~(1 << quantum[k])
+                memory[address] = value
+
+    print(sum(memory.values()))
